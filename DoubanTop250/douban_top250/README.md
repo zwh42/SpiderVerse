80f2c1bacdc91c9f0f6d76d339244e4e9f891395
Wenhao Zhao <zhaowh1987@gmail.com>
2020-04-09 00:52:31 +0800

    add no Scrapy Douban spider

---
+++ DoubanTop250/douban_top250/README.md
@@ -0,0 +1,3 @@
+## to start the crawler
+
+`～ scrapy crawl douban_book_top250`
---
+++ DoubanTop250/douban_top250/douban_top250/pipelines.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+from scrapy.exporters import JsonItemExporter
+
+
+class DoubanTop250Pipeline(object):
+
+    def __init__(self):
+        self._file_path = './parse_result.json'
+        self._file = None
+
+    def open_spider(self, spider):
+        self.file = open(self._file_path, 'wb')
+        self.exporter = JsonItemExporter(
+            self.file, ensure_ascii=False, encoding='utf-8')
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+        print(f"parse result is saved to {self._file_path}")
+
+    def process_item(self, item, spider):
+        print(f"exporting item: {item}")
+        self.exporter.export_item(item)
+        return item
